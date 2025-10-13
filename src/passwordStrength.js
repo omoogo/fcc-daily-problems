36fbfd93bc54c9667fd7b1077635b75f15351d80
Michael Broniola <mbroniola@gmail.com>
2025-10-13 06:25:18 -0700

    Added first problems

--- src/passwordStrength.js
+++ src/passwordStrength.js
@@ -1,9 +1,15 @@
 function checkStrength(password) {
-  if (password.length < 8) {
-    return "weak";
-  }
+  let strengthScore = 0;
+  let hasUpper = false;
+  let hasLower = false;
+  let hasNumber = false;
+  let hasSpecial = false;
 
-  return password;
+  if (password.length >= 8) strengthScore++;
+
+  if (strengthScore < 2) return "weak";
+  if (strengthScore <= 3) return "medium";
+  return "strong"; // anything above 3
 }
 
 module.exports = checkStrength;
