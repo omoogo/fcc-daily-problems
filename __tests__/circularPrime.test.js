2c77985d06b888983830afd68f57ce8050690505
Michael Broniola <mbroniola@gmail.com>
2026-01-10 22:39:54 -0800

    Added circularPrime
    Added code and tests for circularPrime. Also extracted isPrime to utils.js

---
+++ __tests__/circularPrime.test.js
@@ -0,0 +1,21 @@
+const isCircularPrime = require("../src/circularPrime");
+
+test('isCircularPrime(197) should return true', () => {
+    expect(isCircularPrime(197)).toBe(true);
+});
+
+test('isCircularPrime(23) should return false', () => {
+    expect(isCircularPrime(23)).toBe(false);
+});
+
+test('isCircularPrime(13) should return true', () => {
+    expect(isCircularPrime(13)).toBe(true);
+});
+
+test('isCircularPrime(89) should return false', () => {
+    expect(isCircularPrime(89)).toBe(false);
+});
+
+test('isCircularPrime(1193) should return true', () => {
+    expect(isCircularPrime(1193)).toBe(true);
+});
